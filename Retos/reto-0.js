e715a751a12519b8d904769d31d005b3db686409
Nahuel <nahuel.profesor@gmail.com>
2022-12-27 12:51:01 -0300

    Sacando los números correspondientes a fizz, buzz y fizzbuzz

--- Retos/reto-0.js
+++ Retos/reto-0.js
@@ -9,13 +9,13 @@
 
 for(let i = 1; i<= 100 ; i++){
     if(i%3 === 0 && i%5 === 0){
-        console.log(`${i} fizzbuzz`);
+        console.log(`fizzbuzz`);
         console.log(`\n`);
     } else if(i%3 === 0){
-        console.log(`${i} fizz`);
+        console.log(`fizz`);
         console.log(`\n`);
     } else if(i%5 === 0){
-        console.log(`${i} buzz`);
+        console.log(`buzz`);
         console.log(`\n`);
     } else {
         console.log(`${i}`);
